69fc912311716ff02ee6e477dccc9579ff8be5fc
kamil-dev <kamil.przylepa@gmail.com>
2021-01-14 11:10:23 +0100

    you can download

---
+++ eq_world_map_30.py
@@ -0,0 +1,36 @@
+import json
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = 'eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+readable_file = 'readable_eq_data.json'
+with open(readable_file, 'w') as f:
+        json.dump(all_eq_data, f, indent=4)
+all_eq_dicts = all_eq_data['features']
+
+
+mags, lons, lats = [],[], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Sila'}
+    }, }]
+my_layout = Layout(title='Trzesienie Ziemi na Świecie')
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes_30.html')
