d68e74d610c7533d2656602910b9e604189e811a
kamil-dev <kamil.przylepa@gmail.com>
2021-01-15 13:04:45 +0100

    Revert "show add text"
    This reverts commit 6657e1c16845a396cb3b3152e56ebffa7076bc4d.

--- eq_world_map_30.py
+++ eq_world_map_30.py
@@ -11,23 +11,19 @@ with open(readable_file, 'w') as f:
 all_eq_dicts = all_eq_data['features']
 
 
-mags, lons, lats, hover_texts = [],[], [], []
+mags, lons, lats = [],[], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
-    hover_texts.append(title)
-
 
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
-    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],
         'color': mags,
